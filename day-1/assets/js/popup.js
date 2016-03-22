8bd496625a122c2886b96d5848b8ee3b959ad366
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-03-22 18:54:42 +0100

    Ex02: jQuery to native JS

--- day-1/assets/js/popup.js
+++ day-1/assets/js/popup.js
@@ -1,28 +1,14 @@
-$(function () {
-    $('form[name="login"]').submit(function (e) {
-        e.preventDefault();
+window.onload = function () {
+    document.forms['login'].onsubmit = function () {
+        document.querySelector('main').insertAdjacentHTML(
+            'afterend',
+            '<div class="popup"><div>You\'re logged in !</div></div>'
+        );
 
-        if (!$('.popup').length) {
-            $('main').after('<div class="popup"><div>You are logged in !</div></div>');
-        }
-        $('.popup > div').css({
-            position: 'absolute',
-            top: '25%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            backgroundColor: '#cecece',
-            padding: '40px'
-        });
-        $('.popup').css({
-            display: 'none',
-            width: '100vw',
-            height: '100vh',
-            backgroundColor: 'rgba(0, 0, 0, .7)',
-            position: 'relative'
-        }).fadeIn(1000);
+        document.querySelector('.popup').onclick = function () {
+            this.parentNode.removeChild(this);
+        };
 
-        $('body').click(function () {
-            $('.popup').fadeOut(1000);
-        });
-    });
-});
+        return false;
+    };
+};
