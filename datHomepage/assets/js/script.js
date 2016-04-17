f74fa4f05d740c741837448baf904638446c8aaa
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-04-17 22:10:48 +0200

    Add slide down menu

--- datHomepage/assets/js/script.js
+++ datHomepage/assets/js/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-	// Dynamic margin top of the fifth section of main
+    // Dynamic margin top of the fifth section of main
     function sectionMargin() {
         var sectionFix = document.querySelector('main > section:nth-child(5)');
         sectionFix.style.marginTop = -(sectionFix.clientHeight) + 'px';
@@ -10,9 +10,10 @@ window.onload = function () {
     window.onresize = sectionMargin;
 
     // Avoid refreshing of forms
-    for(var i in document.forms) {
-    	document.forms[i].onsubmit = function () {
-    		return false;
-    	};
+    var forms = document.forms;
+    for (var j in forms) {
+        forms[j].onsubmit = function () {
+            return false;
+        };
     }
 };
