12532390906e6e8227243b4de55801871990ab03
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-04-17 17:49:18 +0200

    Update footer

--- datHomepage/assets/js/script.js
+++ datHomepage/assets/js/script.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+	// Dynamic margin top of the fifth section of main
     function sectionMargin() {
         var sectionFix = document.querySelector('main > section:nth-child(5)');
         sectionFix.style.marginTop = -(sectionFix.clientHeight) + 'px';
@@ -7,4 +8,11 @@ window.onload = function () {
     sectionMargin();
 
     window.onresize = sectionMargin;
+
+    // Avoid refreshing of forms
+    for(var i in document.forms) {
+    	document.forms[i].onsubmit = function () {
+    		return false;
+    	};
+    }
 };
